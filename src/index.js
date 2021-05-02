ce516a611dee57bb297e45d4b9a32e6791cf0e91
padunk <abraham.agel@gmail.com>
2021-05-02 18:46:33 +0700

    feat: messages endpoint

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 require("dotenv").config();
 const express = require("express");
+const sqlite3 = require("sqlite3").verbose();
 const verifyWebhook = require("./verify-webhook");
 const { handleMessage, handlePostback } = require("./messages");
 
@@ -10,6 +11,29 @@ const PORT = 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const db = new sqlite3.Database("./db/messages.db", (err) => {
+    if (err) {
+        console.error(err.message);
+        return;
+    }
+
+    // db.serialize(() => {
+    //     db.run(
+    //         "CREATE TABLE messages (user TEXT NOT NULL, message TEXT NOT NULL)"
+    //     );
+    // });
+
+    // let query = `SELECT * FROM messages`;
+    // db.all(query, [], (err, rows) => {
+    //     if (err) {
+    //         throw err;
+    //     }
+    //     rows.forEach((row) => {
+    //         console.log(row);
+    //     });
+    // });
+});
 
 app.get("/webhook", verifyWebhook);
 
@@ -23,10 +47,14 @@ app.post("/webhook", (req, res) => {
             const sender_psid = webhook_event.sender.id;
             console.log("Sender PSID:", sender_psid);
 
+            let query = `INSERT INTO messages (user, message) VALUES (?, ?);`;
+
             if (webhook_event.message) {
                 handleMessage(sender_psid, webhook_event.message);
+                db.run(query, [sender_psid, webhook_event.message.text]);
             } else if (webhook_event.postback) {
                 handlePostback(sender_psid, webhook_event.postback);
+                db.run(query, [sender_psid, webhook_event.postback.payload]);
             }
 
             res.status(200).send("EVENT_RECEIVED");
@@ -34,6 +62,55 @@ app.post("/webhook", (req, res) => {
     } else {
         res.sendStatus(404);
     }
+});
+
+app.get("/messages", (req, res) => {
+    let query = `SELECT * FROM messages`;
+    const result = [];
+    db.all(query, [], (err, rows) => {
+        if (err) {
+            console.log(err);
+            res.send(err.message);
+            return;
+        }
+
+        console.log("rows:", rows);
+        rows.forEach((row) => {
+            result.push({
+                user: row.user,
+                message: row.message,
+            });
+        });
+        res.send(JSON.stringify(result));
+    });
+});
+
+app.get("/messages/:id", (req, res) => {
+    let query = `SELECT * FROM messages WHERE rowid = ?`;
+    const result = [];
+    db.get(query, req.params.id, (err, row) => {
+        if (err) {
+            console.log(err);
+            res.send(err.message);
+            return;
+        }
+        result.push(row);
+        res.send(JSON.stringify(result));
+    });
+});
+
+app.delete("/messages/:id", (req, res) => {
+    let query = `DELETE FROM messages WHERE rowid = ?`;
+
+    db.run(query, req.params.id, (err, row) => {
+        if (err) {
+            console.log(err);
+            res.send(err.message);
+            return;
+        }
+
+        res.send(`Successfully delete message with id: ${req.params.id}`);
+    });
 });
 
 app.listen(PORT, () => {
